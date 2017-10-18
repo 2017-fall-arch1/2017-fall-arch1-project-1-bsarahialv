836aa3df0c4464377500eb5d093ec45859b87ca9
Bianca Alvarez <bsalvarez@miners.utep.edu>
2017-10-17 19:45:25 -0600

    main

---
+++ BTree.h
@@ -0,0 +1,34 @@
+#ifndef llist_included
+#define llist_included
+
+
+
+typedef struct tnode {
+    char *str;
+  struct tnode *left;
+  struct tnode *right;
+
+} tnode;
+
+/*create a new node*/
+  tnode *btAlloc();
+
+/* free memory associated with a node*/
+//void btFree(tnode *b);
+
+/*append a copy of str to end of list*/
+tnode* btPut(tnode *b, char *s);
+
+tnode* newNode (char *s);
+
+/*Delete all elements off the tree*/
+//void btMakeEmpty (tnode *b);
+
+/* print */
+  void btPrint (tnode *b, int depth);
+
+void bPrintPreorder (tnode *b);
+/*check consistency, always returns zero*/
+//int btCheck(tnode *b);
+
+#endif /*included*/
